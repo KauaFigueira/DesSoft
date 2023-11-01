d0f86b2456e8570f23be7567b6d77011f13f6bb0
Kaua <kauafs@al.insper.edu.br>
2023-11-01 15:56:16 -0300

    Função posição

--- Funcoes.py
+++ Funcoes.py
@@ -5,3 +5,18 @@ def filtra(lista,n):
             if i.lower() not in listap:
                 listap.append(i.lower())
     return listap 
+
+def inidica_posicao(ps,pe):
+    lista = []
+    ps = ps.lower()
+    pe = pe.lower()
+    if len(ps) != len(pe):
+        return []
+    for i in range(len(pe)):
+        if pe[i] == ps[i]:
+            lista.append(0)
+        elif pe[i] in ps:
+            lista.append(1)
+        elif pe[i] not in ps:
+            lista.append(2)
+    return lista
